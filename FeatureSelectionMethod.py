fixed normalization; how_we_type data extraction

FeatureSelectionMethod.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class FeatureSelectionMethod(ABC):
     
@@ -48,15 +49,23 @@ class FeatureSelectionMethod(ABC):
 
     @classmethod
     def getData(self, path):
-        with open(path, "r") as f:
-            reader = csv.reader(f)
-            predictors = list(reader)[0]
+        # with open(path, "r") as f:
+        #     reader = csv.reader(f)
+        #     predictors = list(reader)[0]
         
-        alldata = np.loadtxt(open(path, "rb"), delimiter=",", skiprows=1)
-        Xdata = alldata[:, :-1]
-        Ydata = alldata[:, -1]
+        # alldata = np.loadtxt(open(path, "rb"), delimiter="\t", skiprows=1)
+        alldata = pd.read_csv(path, sep="\t")
+        # print(alldata)
+        predictors = alldata.columns
+        # Xdata = alldata[:, :-1]
+        # Ydata = alldata[:, -1]
+        Xdata = alldata.iloc[:, 1:-1]
+        Ydata = alldata.iloc[:, -1]
+        # print(predictors)
+        # print(Xdata)
+        # print(Ydata)
 
-        return predictors, preprocessing.normalize(Xdata), Ydata
+        return predictors[1:], preprocessing.normalize(Xdata, axis=0), Ydata
 
     @classmethod
     def getTrainData(self, data_filename):

main.py
@@ -2,38 +2,53 @@ from FilterMethods import ANOVA, MaximalInformationCoefficient, MutualInformatio
 from WrapperSelectionMethods import ForwardSelection, BackwardSelection, GeneticAlgorithm, RecursiveFeatureElimination, SimulatedAnnealing, StepwiseSelection
 import os
 import sklearn
+import numpy as np
+import csv
+from sklearn import preprocessing
+
+# with open("databases/Keystroke Dynamics – Android platform/Keystroke Dynamics – Android platform.csv", "r") as f:
+#     reader = csv.reader(f)
+#     predictors = list(reader)[0]
+        
+# alldata = np.loadtxt(open("databases/Keystroke Dynamics – Android platform/Keystroke Dynamics – Android platform.csv", "rb"), delimiter=",", skiprows=1)
+# Xdata = alldata[:, :-1]
+# Ydata = alldata[:, -1]
+
+# print(preprocessing.normalize(Xdata, norm="l1"))
+
 
 path_to_files = "extracted/train"
-print('The scikit-learn version is {}.'.format(sklearn.__version__))
+# print('The scikit-learn version is {}.'.format(sklearn.__version__))
 for k in range(5, 15, 2):
     for filename in os.listdir(path_to_files):
-        # print("ANOVA: ", k, " features, ", filename)
-        # print(ANOVA.getKBestFeatures(k, filename))
-        # print()
-        # print("ReliefF: ", k, " features, ", filename)
-        # print(ReliefF.getKBestFeatures(k, filename))
-        # print()
-        # print("MutualInformation: ", k, " features, ", filename)
-        # print(MutualInformation.getKBestFeatures(k, filename))
-        # print()
-        # print("MaximalInformationCoefficient: ", k, " features, ", filename)
-        # print(MaximalInformationCoefficient.getKBestFeatures(k, filename))
-        # print()
-        # print("ForwardSelection: ", k, " features, ", filename)
-        # print(ForwardSelection.getKBestFeatures(k, filename))
-        # print()
-        # print("BackwardSelection: ", k, " features, ", filename)
-        # print(BackwardSelection.getKBestFeatures(k, filename))
-        # print()
+        print("ANOVA: ", k, " features, ", filename)
+        print(ANOVA.getKBestFeatures(k, filename))
+        print()
+        print("ReliefF: ", k, " features, ", filename)
+        print(ReliefF.getKBestFeatures(k, filename))
+        print()
+        print("MutualInformation: ", k, " features, ", filename)
+        print(MutualInformation.getKBestFeatures(k, filename))
+        print()
+        print("MaximalInformationCoefficient: ", k, " features, ", filename)
+        print(MaximalInformationCoefficient.getKBestFeatures(k, filename))
+        print()
+        print("ForwardSelection: ", k, " features, ", filename)
+        print(ForwardSelection.getKBestFeatures(k, filename))
+        print()
+        print("BackwardSelection: ", k, " features, ", filename)
+        print(BackwardSelection.getKBestFeatures(k, filename))
+        print()
         print("StepwiseSelection: ", k, " features, ", filename)
         print(StepwiseSelection.getKBestFeatures(k, filename))
         print()
-        # print("RecursiveFeatureElimination: ", k, " features, ", filename)
-        # print(RecursiveFeatureElimination.getKBestFeatures(k, filename))
-        # print()
+        print("RecursiveFeatureElimination: ", k, " features, ", filename)
+        print(RecursiveFeatureElimination.getKBestFeatures(k, filename))
+        print()
         # print("GeneticAlgorithm: ", k, " features, ", filename)
         # print(GeneticAlgorithm.getKBestFeatures(k, filename))
         # print()
-        # print("SimulatedAnnealing: ", k, " features, ", filename)
-        # print(SimulatedAnnealing.getKBestFeatures(k, filename))
-        # print()
+        print("SimulatedAnnealing: ", k, " features, ", filename)
+        print(SimulatedAnnealing.getKBestFeatures(k, filename))
+        print()
+

preprocess_data.py
@@ -1,10 +1,9 @@
-from importlib.resources import path
 import os
 import csv
 import numpy as np
 import random
-from enum import Enum
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 class dataset():
     X = [[]]
@@ -35,7 +34,7 @@ def txtToCsv():
             f.writelines(lines)
             # break
 
-def extractHowWeTypeData():
+def extractHowWeTypeData0():
     pathToDirTyping = "databases/How We Type/typing/Typing/csv"
     pathToDirMotionCapture = "databases/How We Type/motion_capture/Motion Capture/comas/"
     all_data = []
@@ -70,6 +69,23 @@ def extractHowWeTypeData():
     # with open("out.csv", "w", newline="") as f:
     #     writer = csv.writer(f)
     #     writer.writerows(all_data)
+
+def extractHowWeTypeData():
+    path_to_motion_capture = "databases/How We Type/motion_capture/Motion Capture/Motion Capture"
+    path_to_typing = "databases/How We Type/typing/Typing/Typing"
+    all_user_data_mc = []
+    i = 0
+    for user_file in os.listdir(path_to_motion_capture):
+        user_data_mc = pd.read_csv(path_to_motion_capture + "/" + user_file, skiprows=2, sep="\t", quoting=3, error_bad_lines=False)
+        user_data_mc.dropna(subset=["input_index"], inplace=True)
+        print(user_data_mc["uid"].iloc[0])
+        if 5307 in set(user_data_mc["uid"]):
+            all_user_data_mc = user_data_mc
+        else:
+            all_user_data_mc = pd.concat([all_user_data_mc, user_data_mc], ignore_index=True)
+
+    extracted = all_user_data_mc[["stimulus_index", "input", "finger", "iki", "Hands_L_Cout_x", "Hands_L_Cout_y", "Hands_L_Cout_z", "Hands_R_Cout_x", "Hands_R_Cout_y", "Hands_R_Cout_z", "Hands_L_R2_x", "Hands_L_R2_y", "Hands_L_R2_z", "Hands_L_M1_x", "Hands_L_M1_y", "Hands_L_M1_z", "Hands_L_I1_x", "Hands_L_I1_y", "Hands_L_I1_z", "Hands_L_Cin_x", "Hands_L_Cin_y", "Hands_L_Cin_z", "Hands_L_R1_x", "Hands_L_R1_y", "Hands_L_R1_z", "Hands_L_L1_x", "Hands_L_L1_y", "Hands_L_L1_z", "Hands_L_L2_x", "Hands_L_L2_y", "Hands_L_L2_z", "Hands_L_Aout_x", "Hands_L_Aout_y", "Hands_L_Aout_z", "Hands_L_I2_x", "Hands_L_I2_y", "Hands_L_I2_z", "Hands_R_L2_x", "Hands_R_L2_y", "Hands_R_L2_z", "Hands_L_M2_x", "Hands_L_M2_y", "Hands_L_M2_z", "Hands_L_T2_x", "Hands_L_T2_y", "Hands_L_T2_z", "Hands_R_R3_x", "Hands_R_R3_y", "Hands_R_R3_z", "Hands_L_I3_x", "Hands_L_I3_y", "Hands_L_I3_z", "Hands_L_R4_x", "Hands_L_R4_y", "Hands_L_R4_z", "Hands_L_T1_x", "Hands_L_T1_y", "Hands_L_T1_z", "Hands_L_R3_x", "Hands_L_R3_y", "Hands_L_R3_z", "Hands_L_M4_x", "Hands_L_M4_y", "Hands_L_M4_z", "Hands_L_L4_x", "Hands_L_L4_y", "Hands_L_L4_z", "Hands_L_L3_x", "Hands_L_L3_y", "Hands_L_L3_z", "Hands_L_M3_x", "Hands_L_M3_y", "Hands_L_M3_z", "Hands_R_Win_x", "Hands_R_Win_y", "Hands_R_Win_z", "Hands_R_Cin_x", "Hands_R_Cin_y", "Hands_R_Cin_z", "Hands_R_L1_x", "Hands_R_L1_y", "Hands_R_L1_z", "Hands_L_Win_x", "Hands_L_Win_y", "Hands_L_Win_z", "Hands_L_Wout_x", "Hands_L_Wout_y", "Hands_L_Wout_z", "Hands_R_I1_x", "Hands_R_I1_y", "Hands_R_I1_z", "Hands_L_Ain_x", "Hands_L_Ain_y", "Hands_L_Ain_z", "Hands_R_I2_x", "Hands_R_I2_y", "Hands_R_I2_z", "Hands_R_L3_x", "Hands_R_L3_y", "Hands_R_L3_z", "Hands_R_R2_x", "Hands_R_R2_y", "Hands_R_R2_z", "Hands_R_R1_x", "Hands_R_R1_y", "Hands_R_R1_z", "Hands_R_M3_x", "Hands_R_M3_y", "Hands_R_M3_z", "Hands_R_Wout_x", "Hands_R_Wout_y", "Hands_R_Wout_z", "Hands_R_Ain_x", "Hands_R_Ain_y", "Hands_R_Ain_z", "Hands_R_T2_x", "Hands_R_T2_y", "Hands_R_T2_z", "Hands_R_T3_x", "Hands_R_T3_y", "Hands_R_T3_z", "Hands_R_T1_x", "Hands_R_T1_y", "Hands_R_T1_z", "Hands_R_M1_x", "Hands_R_M1_y", "Hands_R_M1_z", "Hands_R_M2_x", "Hands_R_M2_y", "Hands_R_M2_z", "Hands_R_Aout_x", "Hands_R_Aout_y", "Hands_R_Aout_z", "Hands_R_M4_x", "Hands_R_M4_y", "Hands_R_M4_z", "Hands_R_L4_x", "Hands_R_L4_y", "Hands_R_L4_z", "Hands_R_R4_x", "Hands_R_R4_y", "Hands_R_R4_z", "Hands_L_I4_x", "Hands_L_I4_y", "Hands_L_I4_z", "Hands_L_T4_x", "Hands_L_T4_y", "Hands_L_T4_z", "Hands_R_I3_x", "Hands_R_I3_y", "Hands_R_I3_z", "Hands_R_T4_x", "Hands_R_T4_y", "Hands_R_T4_z", "Hands_R_I4_x", "Hands_R_I4_y", "Hands_R_I4_z", "Hands_L_T3_x", "Hands_L_T3_y", "Hands_L_T3_z", "uid"]]
+    extracted.to_csv("out.csv", sep="\t")
 
 
 def loadGREYCData():
@@ -113,27 +129,51 @@ def loadGREYCData():
 
 def splitDataset(filename, ratio):
 
-    alldata = np.loadtxt(open(filename, "rb"), delimiter=",", skiprows=1)
+    alldata = pd.read_csv(filename, delimiter="\t")
+    alldata = alldata[:, 2:]
+    # alldata = np.loadtxt(open(filename, "rb"), delimiter=",", skiprows=1)
 
-
-    train_data = [[]]
-    test_data = [[]]
+    print(alldata)
+    train_data = []
+    test_data = []
 
     for sample_ in alldata:
+        print(sample_)
         if random.random() <= ratio:
             train_data.append(sample_)
         else:
             test_data.append(sample_)
 
-    with open(filename[:-4] + "TRAIN.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(train_data)
-    with open(filename[:-4] + "TEST.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(test_data)
+def splitHowWeType():
+    df = pd.read_csv("out.csv", sep="\t")
+    df.drop('Unnamed: 0', inplace=True, axis=1)
+    test_samples = random.sample(range(0, 50), 10)
+    test = df.apply(lambda row: row[df["stimulus_index"].isin(test_samples)])
+    # train = df.apply(lambda row: row[df["stimulus_index"].(test_samples)])
+    train = pd.concat([df,test]).drop_duplicates(keep=False)
 
-# splitDataset("databases/extracted/Keystroke Dynamics – Android platform.csv", 0.8)
-# loadGREYCData()
+    test.drop("stimulus_index", inplace=True, axis=1)
+    train.drop("stimulus_index", inplace=True, axis=1)
 
-# txtToCsv()
-extractHowWeTypeData()
+    test.to_csv("extracted/test/HowWetypeTEST.csv", sep="\t")
+    train.to_csv("extracted/train/HowWetypeTRAIN.csv", sep="\t")
+
+splitHowWeType()
+# extractHowWeTypeData()
+    # with open(filename[:-4] + "TRAIN.csv", "w", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(train_data)
+    # with open(filename[:-4] + "TEST.csv", "w", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(test_data)
+# extractHowWeTypeData()
+# print(df.columns)
+# df = df.iloc[: , 1:]
+# print(df)
+
+# for file in os.listdir("extracted/train"):
+#     df = pd.read_csv("extracted/train/" + file, sep=",")
+#     df.to_csv("extracted/train/" + file, sep="\t")
+# for file in os.listdir("extracted/test"):
+#     df = pd.read_csv("extracted/test/" + file, sep=",")
+#     df.to_csv("extracted/test/" + file, sep="\t")